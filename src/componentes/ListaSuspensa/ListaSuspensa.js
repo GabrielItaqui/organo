c8f01ce65dacbbc4a0e694a7e9569c78475c1758
Gabriel Itaqui <itaquips4@gmail.com>
2024-03-16 11:04:22 -0300

    Atualização da aula do dia 16/03

--- src/componentes/ListaSuspensa/ListaSuspensa.js
+++ src/componentes/ListaSuspensa/ListaSuspensa.js
@@ -1,13 +1,15 @@
 import "./ListaSuspensa.css"
 
 export const ListaSuspensa = (props) => {
-    console.log(props.itens)
     return (
         <div className="lista-suspensa">
             <label>
                 {props.label}
             </label>
-            <select>
+            <select
+                onChange={evento => props.aoAlterado(evento.target.value)}
+                required={props.obrigatorio}
+            >
                 {props.itens.map((item, index) => {
                     return <option key={index}>{item}</option>
                 })}
