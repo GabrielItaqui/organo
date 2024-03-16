f279aff30f411fdfe80408d58a41f37a3c0adff7
Gabriel Itaqui <itaquips4@gmail.com>
2024-03-16 12:24:45 -0300

    Modularização de times e preparação da area de colaboradores

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -6,16 +6,6 @@ import "./Formulario.css"
 
 export const Formulario = (props) => {
 
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'UX e Design',
-        'Mobile',
-        'Inovação e Gestão'
-    ]
-
     const [nome, setNome] = useState("");
     const [cargo, setCargo] = useState("");
     const [imagem, setImagem] = useState("");
@@ -59,7 +49,8 @@ export const Formulario = (props) => {
 
                 <ListaSuspensa
                     obrigatorio={true}
-                    label="Time" itens={times}
+                    label="Time" 
+                    itens={props.times}
                     valor={time}
                     aoAlterado={valor => setTime(valor)}
                 />
