fd44b0ccd98412214b64fb4b30c1c3ccdff3bdc3
Gabriel Itaqui <itaquips4@gmail.com>
2024-03-25 20:24:32 -0300

    Upgrade Organo - Mudança de Cor de Time

--- src/componentes/Time/Time.js
+++ src/componentes/Time/Time.js
@@ -1,10 +1,15 @@
 import Colaborador from "../Colaborador"
 import "./Time.css"
 
-export const Time = ({ time, colaboradores, aoDeletar }) => {
+export const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
-
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+            <input
+                onChange={evento => mudarCor(evento.target.value, time.nome)}
+                value={time.corSecundaria}
+                type="color"
+                className="input-cor"
+            />
             <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) =>
